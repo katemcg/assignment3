fac0970d78870b4910bf27402aa2f6a3a8ee4d44
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-28 11:47:31 -0400

    Update predict_text.py

--- code/predict_text.py
+++ code/predict_text.py
@@ -1,1 +1,10 @@
-def predict_categories(): {}
+import numpy as np
+
+def predict_sentiment(review, model):
+    """Predict sentiment on a single movie review."""
+    # Make predictions
+    sentiment_prob = model.predict(review)
+    # Assuming binary classification (positive/negative)
+    predicted_sentiment = 'Positive' if sentiment_prob > 0.5 else 'Negative'
+
+    return predicted_sentiment
