d4ff3809761c78bf61f39cbee5134feca6dc492a
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-28 11:39:03 -0400

    Update front_end.py

--- code/front_end.py
+++ code/front_end.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import tensorflow as tf
 from preprocess_text import preprocessor
-from predict_text import predict_categories
-from model_loader import download_model_from_drive, load_model_from_file
+from predict_text import predict_sentiment
+from model_loader import download_model_from_drive, load_sentiment_model
 
-# Provide a comment like this:
-
-
-### Hi User! Please choose a model between ["Glove150d", "DistillBERT", "GPT2"]
-model_name = "DistillBERT"  # Choice to modify model name from your given list
-
-## Now call appropriate class/function from backend to download relevant weights, instantiate specified model and load the weights, and return the loaded model, ready to predict
+folder_id = ' ' # our google drive folder id
+model_name = input("Please choose a model between ["","",""]: ")
+dest_path = '/path/to/save/model.h5'
+download_model_from_drive(folder_id, model_name, dest_path)
+sentiment_model = load_sentiment_model(dest_path)
 
 # load test csv
 test_data = pd.read_csv("test_sample.csv")
@@ -20,7 +18,7 @@ test_reviews=test_data.review  # reviews to predict on
 preprocessed_data = preprocessor(test_data)
 
 # Make predictions
-predictions = predict_review_categories(preprocessed_data, model_name)
+predictions = predict_sentiment(preprocessed_data, model_name)
 
 # Display predictions
 for test_data, prediction in zip(preprocessed_data, predictions):
