5feafc83bf1d7678954d9238315efa74822a6203
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-30 19:37:21 -0400

    Update predict_text.py

--- code/predict_text.py
+++ code/predict_text.py
@@ -2,6 +2,6 @@ def predict_sentiment(text, model):
     """Predict sentiment of text reviews."""
     # Make predictions
     predictions = model.predict(text)
-    predicted_sentiment = ['Positive' if pred[1] > 0.5 else 'Negative' for pred in predictions]
+    predicted_sentiment = ['positive' if pred[1] > 0.5 else 'negative' for pred in predictions]
 
     return predicted_sentiment
