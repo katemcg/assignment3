59fba3e26a77a7da3ef7f7e83ca02b2a746e70fa
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-30 19:11:32 -0400

    Update predict_text.py

--- code/predict_text.py
+++ code/predict_text.py
@@ -1,10 +1,8 @@
-import numpy as np
-
 def predict_sentiment(text, model):
     """Predict sentiment of text reviews."""
     # Make predictions
     prediction = model.predict(text)
     # Assuming binary classification (positive/negative)
-    predicted_sentiment = 'Positive' if prediction > 0 else 'Negative'
+    predicted_sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
 
     return predicted_sentiment
