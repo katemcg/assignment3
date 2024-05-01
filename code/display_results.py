6c4b1f8cb296b7f166db979829097f7f8a873545
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-30 21:09:30 -0400

    Update and rename model_eval.py to display_results.py

--- code/display_results.py
+++ code/display_results.py
@@ -1,6 +1,15 @@
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def display_table(text, predicted_sentiment, test_data_sentiment):
+    df = pd.DataFrame({
+        'Review': text,
+        'Prediction': predicted_sentiment,
+        'Ground Truth': test_data_sentiment
+        })
+    return df
 
 def evaluate_model(test_data_sentiment, predicted_sentiment):
     # Generate classification report
